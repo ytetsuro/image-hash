964c455a8e7400be07534e163b339e66550192b8
Tetsuro Yoshikawa <tetsuro.yoshikawa@seezoo.co.jp>
2019-08-26 03:23:36 +0900

    fix: remove unnecessary code.

--- src/HashableImageSourceConverter/GlayScaleCalculator/NTSCCoefMethod.ts
+++ src/HashableImageSourceConverter/GlayScaleCalculator/NTSCCoefMethod.ts
@@ -1,6 +1,3 @@
 export default function NTSCCoefMethod(r: Uint8, g: Uint8, b: Uint8): Uint8 {
-    const max = Math.max(r, g, b);
-    const min = Math.min(r, g, b);
-
     return <Uint8>(Math.round((2*r + 4*g + b) / 7));
 };
